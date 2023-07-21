56d6d72fd946add441aa0f6ac6f2225242adc610
CodeDotJS <rushforlinux@gmail.com>
2023-07-21 09:12:17 +0530

    fix: missing url, structure, deletion

--- collection.py
+++ collection.py
@@ -15,13 +15,29 @@ headers = {
     'Connection': 'keep-alive'
 }
 
-base_url = "https://www.moma.org/collection/?=undefined&page={}&direction=fwd"
+base_url = "https://www.moma.org/collection/?utf8=%E2%9C%93&q=&classifications=any&date_begin=Pre-1850&date_end=2023&page={}&direction=fwd"
 image_base_url = "https://www.moma.org"
+
+output_directory = "Files/Collection_Pages"
+
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 
 all_data = {}
 
 start = input("Get data from page: ")
 end = input("Get data till page: ")
+
+def normalize_text(text):
+    normalized = unicodedata.normalize('NFKD', text)
+    return ''.join(c for c in normalized if not unicodedata.combining(c))
+
+async def save_page_data_as_json(page_number, data):
+    filename = os.path.join(output_directory, f"{page_number}_MoMA.json")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
 
 async def fetch_page(session, page_number):
     url = base_url.format(page_number)
@@ -37,7 +53,7 @@ async def fetch_page(session, page_number):
                     artist = ''
                     title = ''
                     year = ''
-                    image_url = ''
+                    image_url = None
                     item_url = "https://www.moma.org" + element.get('href').split('?')[0] if element.get('href') else ''
 
                     span_elements = element.find_all(class_='work--in-list__caption__text')
@@ -51,7 +67,7 @@ async def fetch_page(session, page_number):
                     image_element = element.find('img', class_='picture__img--static')
                     if image_element:
                         image_path = image_element.get('src')
-                        image_url = image_base_url + image_path if image_path else ''
+                        image_url = image_base_url + image_path if image_path else None
 
                     data = {
                         'Artist': artist,
@@ -66,53 +82,57 @@ async def fetch_page(session, page_number):
                 page_key = "Page_{}".format(page_number)
                 all_data[page_key] = page_data
                 print(f"Extracted data from Page {page_number}")
+                await save_page_data_as_json(page_number, page_data)  # Save data to individual page file
             else:
                 print(f"Failed to fetch Page {page_number}. Status code: {response.status}")
     except Exception as e:
         print(f"An error occurred while fetching Page {page_number}: {str(e)}")
 
-def normalize_text(text):
-    normalized = unicodedata.normalize('NFKD', text)
-    return ''.join(c for c in normalized if not unicodedata.combining(c))
-
 async def scrape_pages():
     async with aiohttp.ClientSession() as session:
         tasks = []
         for page_number in range(int(start), int(end) + 1):
-            task = asyncio.ensure_future(fetch_page(session, page_number))
-            tasks.append(task)
-            if len(tasks) == 25:
-                await asyncio.gather(*tasks)
-                tasks = []
+            filename = os.path.join(output_directory, f"{page_number}_MoMA.json")
+            if not os.path.exists(filename):
+                task = asyncio.ensure_future(fetch_page(session, page_number))
+                tasks.append(task)
+                if len(tasks) == 25:
+                    await asyncio.gather(*tasks)
+                    tasks = []
         if tasks:
             await asyncio.gather(*tasks)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(scrape_pages())
 
-async def save_page_data_as_json(page_number, data):
-    filename = f"{page_number}_moma_collection.json"
-    with open(filename, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
-
 async def save_all_data_as_json():
+    existing_data = {}
+    if os.path.exists('collection.json') and os.path.getsize('collection.json') > 0:
+        try:
+            with open('collection.json', 'r') as json_file:
+                existing_data = json.load(json_file)
+        except json.decoder.JSONDecodeError:
+            pass
+
     for page_number, data in all_data.items():
-        await save_page_data_as_json(page_number, data)
+        if page_number in existing_data:
+            existing_data[page_number].extend(data)
+        else:
+            existing_data[page_number] = data
+
+    with open('collection.json', 'w') as json_file:
+        json.dump(existing_data, json_file, indent=4)
 
 loop.run_until_complete(save_all_data_as_json())
 
 complete_data = {}
 for page_number in range(int(start), int(end) + 1):
-    filename = f"Page_{page_number}_moma_collection.json"
-    with open(filename, 'r') as json_file:
-        page_data = json.load(json_file)
-        complete_data.update({f"{page_number}": page_data})
+    filename = os.path.join(output_directory, f"{page_number}_MoMA.json")
+    if os.path.exists(filename):
+        with open(filename, 'r') as json_file:
+            page_data = json.load(json_file)
+            complete_data.update({f"{page_number}": page_data})
 
 with open('collection.json', 'w') as json_file:
     json.dump(complete_data, json_file, indent=4)
 print("\nData merged and saved in collection.json")
-
-for page_number in range(int(start), int(end) + 1):
-    filename = f"Page_{page_number}_moma_collection.json"
-    os.remove(filename)
-print("Removed base pages!")
